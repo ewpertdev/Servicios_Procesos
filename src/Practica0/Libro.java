ba04280ff02c5cf910202736d67ba9ee5cc01723
Fir <xblackcat93@gmail.com>
2024-09-15 15:38:23 +0200

    falta hacer main para hacer pruebas

---
+++ src/Practica0/Libro.java
@@ -0,0 +1,58 @@
+package Practica0;
+
+import java.util.Date;
+
+class Libro {
+    String isbn;
+    String titulo;
+    String autor;
+    int anioPublicacion;
+    boolean disponible = true;
+
+    void Prestamo(){
+
+    }
+    void devolucion() {
+
+    }
+    void informacion () {
+
+    }
+}
+
+class Usuario {
+    String idUsuario;
+    String nombre;
+    String telefono;
+    String email;
+
+    void registrar() {
+        System.out.println("Nuevo usuario registrado");
+
+    }
+    void actualizarInfo() {
+        System.out.println("Información del usuario actualizada");
+    }
+    void consultarPrestamos() {
+
+    }
+}
+
+class Prestamo {
+    String idPrestamo;
+    Date fechaPrestamo;
+    Date fechaDevolucion;
+
+    void realizarPrestamo() {
+
+    }
+    void finalizarPrestamo() {
+
+    }
+}
+
+class Biblioteca {
+    public static void main (String[] args) {
+
+    }
+}
