f4f3052d5b4413be5d9d0629cdbeac3e961db623
ewpertdev <xblackcat93@gmail.com>
2024-09-16 10:53:57 +0200

    añadiendo metodos propios de cada clase

--- src/practica2/Main.java
+++ src/practica2/Main.java
@@ -11,10 +11,18 @@ public class Documento implements Imprimible {
     private String send;
     private String scan;
 
+    public Documento (String send, String scan) {
+
+    }
+
 }
 public class Reporte implements Imprimible {
     private String send;
     private String scan;
+
+    public Reporte (String send, String scan){
+
+    }
 
 }
 
