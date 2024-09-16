57a7d82360751079bd5ec9485dfc1856e84dba50
ewpertdev <xblackcat93@gmail.com>
2024-09-16 14:10:50 +0200

    implementacion

---
+++ src/practica2/Reporte.java
@@ -0,0 +1,15 @@
+package practica2;
+
+public class Reporte implements Imprimible {
+    private String enviar;
+    private String contenido;
+
+    public Reporte (String enviar, String contenido){
+
+    }
+
+    @Override
+    public String imprimir() {
+        return "";
+    }
+}
